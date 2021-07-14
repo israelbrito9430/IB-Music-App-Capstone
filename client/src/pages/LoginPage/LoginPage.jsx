ac56fc2e193c562cb6095dfa915e279590458db9
israelbrito9430 <israelbrito9430@gmail.com>
2021-07-14 17:02:47 -0400

    login page

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from "../../components/Header/Header";
 
 function LoginPage() {
+    const handleClick = () => {
+        console.log('click');
+    };
+
     return (
         <div>
+            <Header />
             <h1>Login</h1>
                 <div>
                     <label htmlFor="username">User Name</label>
@@ -12,6 +18,7 @@ function LoginPage() {
                     <label htmlFor="password">Password</label>
                     <input type="password" name="Password"/>
                 </div>
+                <button onClick={handleClick}>Login</button>
         </div>
     )
 }
