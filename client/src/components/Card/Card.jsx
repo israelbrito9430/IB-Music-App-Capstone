5aff76b3ec5aa86adabb44fbe7d67dbfe402b6a3
israelbrito9430 <israelbrito9430@gmail.com>
2021-07-14 15:47:04 -0400

    welcome page cards

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-import "./CardComponent.scss";
+import "./Card.scss";
 
 function CardComponent(props) {
 
--- client/src/pages/WelcomePage/WelcomePage.jsx
+++ client/src/pages/WelcomePage/WelcomePage.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import WelcomePic from "../../assets/WelcomePic.jpg"
-import "./WelcomePage.scss"
+import "./WelcomePage.scss";
+
 import registration_background from "../../assets/registration_background.jpeg"
 import ListentoMusic from "../../assets/ListentoMusic.jpeg"
 import LoginPic from "../../assets/LoginPic.jpg"
-import CardComponent from "../../components/CardWelcomePage/CardComponent"
+import Card from "../../components/Card/Card"
 
 
 function WelcomePage() {
@@ -12,9 +12,9 @@ function WelcomePage() {
         <div className="wpcontainer">
             <h1>IB Music App</h1>
             <div className="wpcontainer__cardlist">
-                <CardComponent title="REGISTRATION" img={registration_background} link="/register" />                
-                <CardComponent title="LISTEN TO MUSIC" img={ListentoMusic} link="/music" />
-                <CardComponent title="LOGIN" img={LoginPic} link="/login" />
+                <Card title="REGISTRATION" img={registration_background} link="/register" />                
+                <Card title="LISTEN TO MUSIC" img={ListentoMusic} link="/music" />
+                <Card title="LOGIN" img={LoginPic} link="/login" />
             </div>
         </div>
     );
