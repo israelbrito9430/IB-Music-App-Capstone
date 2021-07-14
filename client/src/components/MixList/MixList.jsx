9c52934c8541b9bf90b1a914e2472adbd597c64b
israelbrito9430 <israelbrito9430@gmail.com>
2021-07-14 18:10:39 -0400

    Mix List frame

---
+++ client/src/components/MixList/MixList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './MixList.scss';
+
+function MixList() {
+    return (    
+      <div className="mixlist">
+        
+        <div className="mixlist__card">
+          <div>
+            imagen
+          </div>
+          <span>Receta Mix</span>
+          <div>
+            <span>icon</span>
+            <span>Likes</span>
+          </div>
+          <div>
+            imagen play
+          </div>
+        </div>
+
+      </div>
+    )
+}
+
+export default MixList;
