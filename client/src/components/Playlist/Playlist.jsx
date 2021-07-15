8c5f54495df177d71de9f2f81c8a1c122de9b9c3
israelbrito9430 <israelbrito9430@gmail.com>
2021-07-14 22:04:34 -0400

    playList form

---
+++ client/src/components/Playlist/Playlist.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PlayList.scss';
+
+function PlayList() {
+    return (    
+      <div className="playList">
+        <div>icon</div>
+        <div>name</div>
+      </div>
+    )
+}
+
+export default PlayList;
--- client/src/pages/MusicPage/MusicPage.jsx
+++ client/src/pages/MusicPage/MusicPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Header from "../../components/Header/Header";
+import MixList from '../../components/MixList/MixList';
+import PlayList from '../../components/PlayList/PlayList';
+import MixForm from '../../components/MixForm/MixForm';
 
 function MusicPage() {
     return (
         <div>
+            <Header />
             <h1>LIstening music</h1>
+
+            <div>
+                <MixForm />
+            </div>
+
+            <div>
+                <MixList />
+                <MixList />
+                <MixList />
+                <MixList />
+                <MixList />
+            </div>
+            <div>
+                <PlayList />
+            </div>
         </div>
     )
 }
