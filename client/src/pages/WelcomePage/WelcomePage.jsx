ea8e29cf38391eafcace5de5d9e300fbd9e11088
israelbrito9430 <israelbrito9430@gmail.com>
2021-07-15 16:12:28 -0400

    welcome page scss(almost there)

--- client/src/pages/WelcomePage/WelcomePage.jsx
+++ client/src/pages/WelcomePage/WelcomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./WelcomePage.scss";
 
 import registration_background from "../../assets/registration_background.jpeg"
-import ListentoMusic from "../../assets/ListentoMusic.jpeg"
+import Nomusic_Nolife from "../../assets/Nomusic_Nolife.jpeg"
 import LoginPic from "../../assets/LoginPic.jpg"
 import Card from "../../components/Card/Card"
 
@@ -10,10 +10,10 @@ import Card from "../../components/Card/Card"
 function WelcomePage() {
     return (
         <div className="wpcontainer">
-            <h1>IB Music App</h1>
+            <h1 className="wpcontainer__title"> IB Music App</h1>
             <div className="wpcontainer__cardlist">
                 <Card title="REGISTRATION" img={registration_background} link="/register" />                
-                <Card title="LISTEN TO MUSIC" img={ListentoMusic} link="/music" />
+                <Card title="LISTEN TO MUSIC" img={Nomusic_Nolife} link="/music" />
                 <Card title="LOGIN" img={LoginPic} link="/login" />
             </div>
         </div>
