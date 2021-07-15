8c957df20db9d7352916e8b86f9d5865ae5b81b6
israelbrito9430 <israelbrito9430@gmail.com>
2021-07-14 20:56:14 -0400

    mix form

---
+++ client/src/components/MixForm/MixForm.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './MixForm.scss';
+
+function MixForm() {
+    return (    
+      <div className="mixform">
+          Mixform
+        <div className="MixForm__card">
+          <div>
+            imagen
+          </div>
+          <span>Receta Mix</span>
+          <div>
+            <span>icon</span>
+            <span>Likes</span>
+          </div>
+          <div>
+            imagen play
+          </div>
+        </div>
+
+      </div>
+    )
+}
+
+export default MixForm;
