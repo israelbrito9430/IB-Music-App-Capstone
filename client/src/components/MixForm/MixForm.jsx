333982d458298e63ae147103330bbc2943da9e29
israelbrito9430 <israelbrito9430@gmail.com>
2021-07-16 10:31:29 -0400

    Mix-Form and footer in music page

--- client/src/components/MixForm/MixForm.jsx
+++ client/src/components/MixForm/MixForm.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import './MixForm.scss';
 
-function MixForm() {
-    return (    
+import CommentForm from '../CommentForm/CommentForm';
+
+import likesIcon from '../../assets/icons/Icon-likes.svg';
+import viewIcon from '../../assets/icons/Icon-views.svg';
+import playIcon from '../../assets/icons/icon-play.png';
+import camiloSong from '../../assets/music/Camilo-RopaCara.mp3';
+
+function MixForm(props) {
+    return (
+      <>
       <div className="mixform">
-          Mixform
-        <div className="MixForm__card">
+        
+        <div className="mixform__card">
           <div>
-            imagen
-          </div>
-          <span>Receta Mix</span>
-          <div>
-            <span>icon</span>
-            <span>Likes</span>
+              <img src={props.img} />
           </div>
           <div>
-            imagen play
+            <div>{props.title}</div>
+            <div>
+              <audio class="my_audio" controls preload="none">
+                <source src={camiloSong} type="audio/mpeg" />
+                <source src="audio/my_song.ogg" type="audio/ogg" />
+              </audio>
+            </div>
+          </div>
+          <div>
+            {/* <img className="icon-play" src={playIcon} alt="Likes Icon" /> */}
+            <img className="icon" src={likesIcon} alt="Likes Icon" />
+            <img className="icon" src={viewIcon} alt="View Icon" />
           </div>
         </div>
-
       </div>
+      <CommentForm />
+      </>
     )
 }
 
--- client/src/pages/MusicPage/MusicPage.jsx
+++ client/src/pages/MusicPage/MusicPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Header from "../../components/Header/Header";
 import MixList from '../../components/MixList/MixList';
-import PlayList from '../../components/PlayList/PlayList';
+import PlayList from '../../components/Playlist/PlayList';
 import MixForm from '../../components/MixForm/MixForm';
+import Footer from '../../components/Footer/Footer';
 
 function MusicPage() {
     return (
@@ -11,19 +12,20 @@ function MusicPage() {
             <h1>LIstening music</h1>
 
             <div>
-                <MixForm />
+                <MixForm title="Camilo - Ropa Cara" img="https://via.placeholder.com/100" />
             </div>
 
-            <div>
-                <MixList />
-                <MixList />
-                <MixList />
-                <MixList />
-                <MixList />
+            <div style={{ display: 'flex', flexDirection: 'column', paddingTop: '40px'}}>
+                <MixList title="Regueton Mix" img="https://via.placeholder.com/100" />
+                <MixList title="Pop Mix" img="https://via.placeholder.com/100" />
+                <MixList title="Salsa Mix" img="https://via.placeholder.com/100" />
+                <MixList title="Vallenato Mix" img="https://via.placeholder.com/100" />
+                <MixList title="Rock Mix" img="https://via.placeholder.com/100" />
             </div>
             <div>
                 <PlayList />
             </div>
+            <Footer />
         </div>
     )
 }
