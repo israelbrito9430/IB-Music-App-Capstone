e7e768441a490fa132100f7217c3dcfdbed6de38
israelbrito9430 <israelbrito9430@gmail.com>
2021-07-16 00:58:27 -0400

    mix list

--- client/src/components/MixList/MixList.jsx
+++ client/src/components/MixList/MixList.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import './MixList.scss';
 
-function MixList() {
+import likesIcon from '../../assets/icons/Icon-likes.svg';
+import playIcon from '../../assets/icons/icon-play.png';
+
+
+function MixList(props) {
     return (    
       <div className="mixlist">
         
         <div className="mixlist__card">
           <div>
-            imagen
-          </div>
-          <span>Receta Mix</span>
-          <div>
-            <span>icon</span>
-            <span>Likes</span>
+              <img src={props.img} />
           </div>
           <div>
-            imagen play
+            <div>{props.title}</div>
+            <div>
+              <img src={likesIcon} alt="Likes Icon" />
+              <span>Likes</span>
+            </div>
+          </div>
+          <div>
+            <img className="icon-play" src={playIcon} alt="Likes Icon" />
           </div>
         </div>
 
