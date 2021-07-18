9213c693d87ba8ec3d2c33e18255a1e9d7f9651c
israelbrito9430 <israelbrito9430@gmail.com>
2021-07-18 13:55:59 -0400

    login page firebase

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,28 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from "react-router-dom";
+
+import 'firebase/auth';
+import { useFirebaseApp, useUser } from 'reactfire';
+
 import Header from "../../components/Header/Header";
 
 function LoginPage() {
     const history = useHistory();
 
-    const handleClick = () => {
-        console.log('click');
+    const firebase = useFirebaseApp();
+    const user = useUser();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    useEffect(() => {
+        if(user.data && user.data.email) {
+            history.push("/user");
+        }
+    }, []);
+
+    const handleClick = async () => {
+        //create new user on firebase auth
+        await firebase.auth().createUserWithEmailAndPassword(email, password);
+    };
+
+    const handleLogin = async () => {
+        await firebase.auth().signInWithEmailAndPassword(email, password);
         history.push("/user");
     };
+
+    const handleLogout = async () => {
+        await firebase.auth().signOut();
+    };
+
+    // console.log('user: ', user);
 
     return (
         <div>
             <Header />
             <h1>Login</h1>
-                <div>
-                    <label htmlFor="username">User Name</label>
-                    <input type="text" name="username" />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="Password"/>
-                </div>
-                <button onClick={handleClick}>Login</button>
+            { user.data?.email && <button onClick={handleLogout}>Logout</button>}
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" name="Password" onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <button onClick={handleLogin}>Login</button>
         </div>
     )
 }
